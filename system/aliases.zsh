8f0c68d57825cb9bf609bc7311a70cc46f386969
AUTHORNAME <AUTHOREMAIL>
2017-02-20 14:57:19 -0600

    Integrate oh-my-zsh (clumsily)

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -4,7 +4,9 @@
 if $(gls &>/dev/null)
 then
   alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
+  alias ll="gls -lAh --color"
+  alias l="gls -lFh --color"
   alias la='gls -A --color'
 fi
+
+alias be="bundle exec"
