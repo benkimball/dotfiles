3609eb6ec5920021f7cac2052ddf1ea60aa1b6cb
Ben Kimball <benkimball@mac.com>
2017-02-22 14:57:30 -0600

    update git aliases, remove subl alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,8 +1,3 @@
 alias gc='git commit'
 alias gca='git commit -a'
-alias gco='git checkout'
-alias gb='git branch'
-alias gst='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
-
-alias subl='nocorrect subl'
+alias gst='git status'
