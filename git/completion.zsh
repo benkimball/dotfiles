0d23e74b06b9a86ce42c3e9d166fe074b3521249
Ben Kimball <benkimball@mac.com>
2017-03-02 16:41:01 -0600

    First attempt hacking git completion for grb

--- git/completion.zsh
+++ git/completion.zsh
@@ -7,3 +7,17 @@ if test -f $completion
 then
   source $completion
 fi
+
+_git_remote_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null)
+  if [[ -n $ref ]]; then
+    if (( CURRENT == 2 )); then
+      compadd create new publish remotize share delete destroy kill remove rm rename rn mv move track follow grab fetch
+    elif (( CURRENT == 3 )); then
+      compadd `git branch | sed "s/[\* ]//g"`
+    fi
+  else;
+    _files
+  fi
+}
+compdef _git_remote_branch grb
