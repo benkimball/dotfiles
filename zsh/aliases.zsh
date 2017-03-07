c7663f70657d722d5053a31e52213622e1b46202
Ben Kimball <benkimball@mac.com>
2017-03-07 12:10:56 -0600

    neato console aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,6 @@ alias reload!='. ~/.zshrc'
 
 alias ohmyzsh="mate $ZSH"
 alias dotfiles="mate $DOTFILES_ZSH"
+
+alias prod-console="pushd ~/code/borrowed-and-blue; heroku console -r staging; popd"
+alias test-console="pushd ~/code/borrowed-and-blue; heroku console -r test; popd"
