5f157afb952f7bb2129de0e64d2eb75825bba9bc
Ben Kimball <benkimball@mac.com>
2017-04-12 13:50:01 -0500

    nethack alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,5 @@ alias dotfiles="mate $DOTFILES_ZSH"
 
 alias prod-console="pushd ~/code/borrowed-and-blue; heroku console -r staging; popd"
 alias test-console="pushd ~/code/borrowed-and-blue; heroku console -r test; popd"
+
+alias ng=nethack
