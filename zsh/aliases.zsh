1c0d6204af4eac27d9f03406217554bf9f80c7a9
Ben Kimball <benkimball@mac.com>
2017-04-20 11:55:45 -0500

    replace ; with && probably to no purpose

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,7 +3,7 @@ alias reload!='. ~/.zshrc'
 alias ohmyzsh="mate $ZSH"
 alias dotfiles="mate $DOTFILES_ZSH"
 
-alias prod-console="pushd ~/code/borrowed-and-blue; heroku console -r staging; popd"
-alias test-console="pushd ~/code/borrowed-and-blue; heroku console -r test; popd"
+alias prod-console="pushd ~/code/borrowed-and-blue && heroku console -r staging && popd"
+alias test-console="pushd ~/code/borrowed-and-blue && heroku console -r test && popd"
 
 alias nh=nethack
