e6a3aef4e27fc67830f10f624942b747b250361e
Ben Kimball <ben.kimball@youearnedit.com>
2018-03-08 10:30:15 -0600

    Remove oh-my-zsh, p function

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,9 +3,4 @@ alias reload!='. ~/.zshrc'
 alias ohmyzsh="mate $ZSH"
 alias dotfiles="mate $DOTFILES_ZSH"
 
-alias prod-console="pushd ~/code/borrowed-and-blue && heroku console -r staging && popd"
-alias test-console="pushd ~/code/borrowed-and-blue && heroku console -r test && popd"
-
-alias nh=nethack
-
 alias fin="open ~/code/financial/index.html -a 'Google Chrome'"
