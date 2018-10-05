a86ccb9c2b7eeec712a5b30159a4afaf235114ff
Ben Kimball <ben.kimball@youearnedit.com>
2018-10-05 14:18:54 -0500

    Latest changes

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,3 @@
 export PATH="~/bin:./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES_ZSH/bin:/usr/local/opt/go/libexec/bin:$PATH"
+export PATH="/usr/local/opt/node@8/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
